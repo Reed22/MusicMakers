80a6b15a783d5234268ca713416e694b5d475657
Reed Robinson <robinree@oregonstate.edu>
2020-10-23 16:08:22 -0400

    added keybinding to piano

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import keydown from 'react-keydown';
 
 function Key(props) {
-  const keyColor = props.type === "key white" ? "white" : "black"
-  const playedColor = keyColor === "black" ? "#333" : "#CCC"
+  const keyColor = props.type === 'key white' ? 'white' : 'black';
+  const playedColor = keyColor === 'black' ? '#333' : '#CCC';
 
-  const [played, setPlayed] = useState(false)
-  const [color, setColor] = useState(keyColor)
+  const [played, setPlayed] = useState(false);
+  const [color, setColor] = useState(keyColor);
 
-  function changeColor(){
+  function changeColor() {
     if (played) {
       setColor(playedColor);
     } else {
@@ -20,22 +21,35 @@ function Key(props) {
     changeColor();
   }
 
-  useEffect(changeColor, [played]);
-
   function playSound() {
     changePlay();
     props.audio.triggerAttackRelease(props.dataNote + props.octave, '8n');
     setTimeout(changePlay, 250); // Probably not the best way to do this
   }
 
+  useEffect(changeColor, [played]);
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode == props.keyCode) playSound();
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <div
-      style={{backgroundColor: color}}
+      style={{ backgroundColor: color}}
       dataNote={props.dataNote}
       className={props.type}
       onClick={playSound}
-      onKeyDown={playSound}
-    />
+      onKeyDown={handleKeyDown}
+    > 
+    <p>{props.keyChar}</p>
+    </div>
   );
 }
 
