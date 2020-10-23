3a39b1f5a5bae3ce1815a07bb3d956d918b3371b
Reed Robinson <robinree@oregonstate.edu>
2020-10-23 12:52:07 -0400

    key color changes when clicked

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Key(props) {
+  const keyColor = props.type === "key white" ? "white" : "black"
+  const playedColor = keyColor === "black" ? "#333" : "#CCC"
+
+  const [played, setPlayed] = useState(false)
+  const [color, setColor] = useState(keyColor)
+
+  function changeColor(){
+    if (played) {
+      setColor(playedColor);
+    } else {
+      setColor(keyColor);
+    }
+  }
+
+  function changePlay() {
+    setPlayed((prevPlay) => !prevPlay);
+    changeColor();
+  }
+
+  useEffect(changeColor, [played]);
+
   function playSound() {
+    changePlay();
     props.audio.triggerAttackRelease(props.dataNote + props.octave, '8n');
+    setTimeout(changePlay, 250); // Probably not the best way to do this
   }
 
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div
-      // eslint-disable-next-line react/destructuring-assignment
+      style={{backgroundColor: color}}
       dataNote={props.dataNote}
-      // eslint-disable-next-line react/destructuring-assignment
       className={props.type}
       onClick={playSound}
       onKeyDown={playSound}
