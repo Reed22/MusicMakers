f1f8dadc1a66fc446e770729a37184df7b3a2f93
Reed Robinson <robinree@oregonstate.edu>
2020-10-30 22:56:47 -0400

    Expanded keyboard to 24 keys, added side arrows to change octaves. Changed keyboard input

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Tone from 'tone';
-import Key from './Key';
-import OctaveCtrl from './OctaveCtrl';
+import Octave from './Octave';
 
 function Piano() {
-  // Start at third octave
   const [octave, setOctave] = useState(3);
+  const [octaveLimitLow, setOctaveLimitLow] = useState(false)
+  const [octaveLimitHigh, setOctaveLimitHigh] = useState(false)
 
-  // Sound samples for piano. May need to set as a buffer with Tone buffer
+  const leftOctaveKeys = ['Z', 'S', 'X', 'D', 'C', 'V', 'G', 'B', 'H', 'N', 'J', 'M']
+  const rightOctaveKeys = ['W', '3', 'E', '4', 'R', 'T', '6', 'Y', '7', 'U', '8', 'I']
+
   const sampler = new Tone.Sampler({
     urls: {
       A0: 'A0.mp3',
@@ -44,129 +46,57 @@ function Piano() {
     release: 1,
     baseUrl: 'https://tonejs.github.io/audio/salamander/',
   }).toDestination();
-
+/*
   const synth = new Tone.Synth();
   synth.oscillator.type = 'sine';
-  synth.toMaster();
+  synth.toMaster();*/
 
-  function changeOctave(event){
-    setOctave(prevState => {
-      // If button pressed was specific octave
-      if(event.target.id != '+1' && event.target.id != '-1')
-        return event.target.id
-      
-      // If button was a decrease or increase from current value. Valid Octave (1 - 4)
-      else if(prevState + parseInt(event.target.id) >= 1 && prevState + parseInt(event.target.id) <= 4)
-          return prevState + parseInt(event.target.id)
-      
-      else return prevState
-      //if(prevState + parseInt(event.target.id) < 1)
+  useEffect(() => {
+    if(octave - 1 == 0)
+      setOctaveLimitLow(true)
+    else if(octave + 1 == 5)
+      setOctaveLimitHigh(true)
+    else {
+      setOctaveLimitHigh(false)
+      setOctaveLimitLow(false)
+    }
+  }, [octave])
+/*
+  function incrementOctave(event){
+    setOctave(prevOctave => {
+      if(prevOctave + 1 < 5)
+        return prevOctave + 1
+      else
+        return prevOctave
     })
+  }
+
+  function decrementOctave(event){
+    setOctave(prevOctave => {
+      if(prevOctave - 1 > 0)
+        return prevOctave - 1
+      else
+        return prevOctave
+    })
+  }*/
+  function incrementOctave(event){
+    setOctave(prevOctave => prevOctave + 1)
+  }
+
+  function decrementOctave(event){
+    setOctave(prevOctave => prevOctave - 1)
   }
 
   return (
     <div>
-      <OctaveCtrl handleChangeOctave={changeOctave} octave={octave} />
-
       <div className="piano">
-        <Key
-          keyChar="S"
-          keyCode="83"
-          octave={octave}
-          audio={sampler}
-          dataNote="C"
-          type="key white"
-        />
-        <Key
-          keyChar="E"
-          keyCode="69"
-          octave={octave}
-          audio={sampler}
-          dataNote="Db"
-          type="key black"
-        />
-        <Key
-          keyChar="D"
-          keyCode="68"
-          octave={octave}
-          audio={sampler}
-          dataNote="D"
-          type="key white"
-        />
-        <Key
-          keyChar="R"
-          keyCode="82"
-          octave={octave}
-          audio={sampler}
-          dataNote="E"
-          type="key black"
-        />
-        <Key
-          keyChar="F"
-          keyCode="70"
-          octave={octave}
-          audio={sampler}
-          dataNote="Eb"
-          type="key white"
-        />
-        <Key
-          keyChar="G"
-          keyCode="71"
-          octave={octave}
-          audio={sampler}
-          dataNote="F"
-          type="key white"
-        />
-        <Key
-          keyChar="Y"
-          keyCode="89"
-          octave={octave}
-          audio={sampler}
-          dataNote="Gb"
-          type="key black"
-        />
-        <Key
-          keyChar="H"
-          keyCode="72"
-          octave={octave}
-          audio={sampler}
-          dataNote="G"
-          type="key white"
-        />
-        <Key
-          keyChar="U"
-          keyCode="85"
-          octave={octave}
-          audio={sampler}
-          dataNote="Ab"
-          type="key black"
-        />
-        <Key
-          keyChar="J"
-          keyCode="74"
-          octave={octave}
-          audio={sampler}
-          dataNote="A"
-          type="key white"
-        />
-        <Key
-          keyChar="I"
-          keyCode="73"
-          octave={octave}
-          audio={sampler}
-          dataNote="Bb"
-          type="key black"
-        />
-        <Key
-          keyChar="K"
-          keyCode="75"
-          octave={octave}
-          audio={sampler}
-          dataNote="B"
-          type="key white"
-        />
+        <button disabled={octaveLimitLow} type="button" className="btn btn-secondary" id="increment-octave-btn" onClick={decrementOctave}>&lt;</button>
+        <Octave keyInput={leftOctaveKeys} octave={octave - 1} sampler={sampler}/>
+        <Octave keyInput={rightOctaveKeys} octave={octave} sampler={sampler}/>
+        <button disabled={octaveLimitHigh} type="button" className="btn btn-secondary" id="-1" onClick={incrementOctave}>&gt;</button>
       </div>
     </div>
+
   );
 }
 
