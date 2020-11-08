23904d3bfadba7e369d3ab86b780bb7a876539c2
Reed Robinson <robinree@oregonstate.edu>
2020-11-08 11:13:37 -0500

    added button that plays C Major Scale

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Octave from './Octave';
+import * as Tone from 'tone';
 import SamplerContext from './SamplerContext'
-import {sampler, leftOctaveKeys, rightOctaveKeys} from './constants'
+import {sampler, leftOctaveKeys, rightOctaveKeys, scales} from './constants'
 
 function Piano() {
   const [octave, setOctave] = useState(3);
@@ -26,13 +27,23 @@ function Piano() {
   function decrementOctave(event){
     setOctave(prevOctave => prevOctave - 1)
   }
+  
+  function playCMajor(scale_array){
+    const now = Tone.now()
+    let delay = 0
+    scale_array.forEach(key => {
+      sampler.triggerAttackRelease(key.note + (octave + key.octaveChange), "8n", now + delay)
+      delay += 0.5
+    })
+  }
 
   return (
     <SamplerContext.Provider value={sampler}>
+      <button onClick={() => playCMajor(scales.cMajor)}>Play C Major</button>
       <div className="piano">
         <button disabled={octaveLimitLow} type="button" className="btn btn-secondary" id="increment-octave-btn" onClick={decrementOctave}>&lt;</button>
-        <Octave keyInput={leftOctaveKeys} octave={octave - 1} />
-        <Octave keyInput={rightOctaveKeys} octave={octave} />
+        <Octave scales={scalePlay} keyInput={leftOctaveKeys} octave={octave - 1} />
+        <Octave scales={scalePlay} keyInput={rightOctaveKeys} octave={octave} />
         <button disabled={octaveLimitHigh} type="button" className="btn btn-secondary" id="-1" onClick={incrementOctave}>&gt;</button>
       </div>
     </ SamplerContext.Provider>
