80ba2ccc387430173dddd92d0b8c38b907b453db
Reed Robinson <robinree@oregonstate.edu>
2020-11-29 18:05:22 -0500

    fixed issue where the backend was crashing when registering accounts

--- services/usersService.js
+++ services/usersService.js
@@ -5,11 +5,11 @@ const post = async (req, res, next) => {
   // TODO validate req body
 
   //If email is already in db, do not allow them to create account.
-  const response = await usersModel.getUserByEmail(req.body.email)
+/*   const response = await usersModel.getUserByEmail(req.body.email)
   if(response.email){
     console.log("same email foud")
     //res.status(400).send({"Error": "Account with this email already exists"})
-  }
+  } */
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const dbRes = usersModel.post({
