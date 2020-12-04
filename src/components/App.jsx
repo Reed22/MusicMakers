0575a92a2d916f6d606a9cc43c6dd1dc94bd15e6
Dominic Lupo <dominiclupo5318@gmail.com>
2020-12-04 18:15:59 -0500

    polish piano and straighten scale choices

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './Header';
-import Quiz from './Quiz';
-import Login from './Login';
-import Home from './Home';
-import GenericMessage from './GenericMessage';
-import CreateScales from './CreateScales'
-import TriadQuiz from './TriadQuiz';
-import IntervalQuiz from './IntervalQuiz';
-import Profile from './Profile'
+import Header from "./Header";
+import Quiz from "./Quiz";
+import Login from "./Login";
+import Home from "./Home";
+import GenericMessage from "./GenericMessage";
+import CreateScales from "./CreateScales";
+import Profile from "./Profile";
 
 export default function App() {
   return (
@@ -17,10 +15,25 @@ export default function App() {
       <div>
         <Header />
         <Switch>
-          <Route exact path="/login" key="/login" render={(props) => <Login {...props} />} />
-          <Route exact path="/profile" key="/profile" render={(props) => <Profile {...props}/>} />
+          <Route
+            exact
+            path="/login"
+            key="/login"
+            render={(props) => <Login {...props} />}
+          />
+          <Route
+            exact
+            path="/profile"
+            key="/profile"
+            render={(props) => <Profile {...props} />}
+          />
           <Route exact path="/quiz" key="/quiz" render={() => <Quiz />} />
-          <Route exact path="/create-scales" key="/create-scales" render={() => <CreateScales />} />
+          <Route
+            exact
+            path="/create-scales"
+            key="/create-scales"
+            render={() => <CreateScales />}
+          />
           <Route exact path="/" key="/" render={() => <Home />} />
           <Route path="*" render={() => <GenericMessage message="404" />} />
         </Switch>
